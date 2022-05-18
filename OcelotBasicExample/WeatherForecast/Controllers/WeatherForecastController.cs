99b9471f6b9ae193fb5cbaf60fcf5e6bf01f71fe
oriol.orra <oriol.orra@es.abb.com>
2022-05-18 11:38:06 +0200

    first commit

---
+++ OcelotBasicExample/WeatherForecast/Controllers/WeatherForecastController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPIExample.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpPost("{weather}")]
+        public ActionResult Post([FromRoute] string weather)
+        {
+            return Ok(weather);
+        }
+        [Route("GetWeather")]
+        [HttpPost]
+        public ActionResult PostQuery([FromQuery] string weather)
+        {
+            return Ok(weather);
+        }
+
+        [Route("ping")]
+        [HttpGet]
+        public ActionResult Ping()
+        {
+            return Ok();
+        }
+    }
+}
